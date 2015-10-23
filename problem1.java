cf815d64700ef231b2d2edf960086859ab0a495e
becksteadn <becksteadn@gmail.com>
2015-10-23 17:41:15 -0400

    Update problem1.java

--- problem1.java
+++ problem1.java
@@ -1,18 +1,18 @@
 public class problem1{
 
-  public static void main(String[] args){
+	  public static void main(String[] args){
 
-          int sum = 0;
+	          int sum = 0;
 
-          for(int i = 0;i< 1000;i++){
-                  if(i%3 == 0 || i%5 == 0){
-                          sum += i;
-                          System.out.println(i);
-                  }
-          }
+	          for(int i = 0;i< 1000;i++){ //every number to 1,000
+	                  if(i%3 == 0 || i%5 == 0){ //if the number if divisble by 3 or 5
+	                          sum += i; //add the number to the sum
+	                          System.out.println(i); //display the number for debugging 
+	                  }
+	          }
 
-          System.out.println("Sum is: " + sum);
+	          System.out.println("Sum is: " + sum); //display the final sum
 
-  }
+	  }
 
-}
+	}
