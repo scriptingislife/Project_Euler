25fb21392a89c57b32ae2a35bf7fc12cc97d1f01
becksteadn <becksteadn@gmail.com>
2015-10-24 11:19:39 -0400

    Problem 5

---
+++ problem5.java
@@ -0,0 +1,26 @@
+public class problem5 {
+
+	public static void main(String[] args){
+		
+		int smallNum = Integer.MAX_VALUE;
+		boolean test = true;
+		
+		for(int i = Integer.MAX_VALUE; i > 0; i--){ //All possible numbers, high-low
+			test = true; //Test variable
+			for(int j = 1; j <= 20; j++){ //Test all numbers 1-20
+				if(i%j != 0){//test if num is divible by current number
+					test = false; //Num is not divisible by the current number 1-20
+				}
+			}//1-20
+			if(test == true){ //if num is divisble by all numbers
+				if(i < smallNum){ //make sure num is smallest
+					smallNum = i; //reset smallest number
+				}
+			}
+		}//num
+		
+		System.out.println(smallNum); //Here's our answer
+		
+	}//main
+	
+}
