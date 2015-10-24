46a98e39d2e1e5078e5d527bd6be8c330fce06e8
becksteadn <becksteadn@gmail.com>
2015-10-24 11:28:12 -0400

    Create problem6.java

---
+++ problem6.java
@@ -0,0 +1,18 @@
+public class problem6 {
+
+	public static void main(String[] args){
+		
+		int sumSquares = 0, squareSum = 0;
+		
+		for(int i = 1; i <=100; i++){ //numbers 1-100
+			squareSum += i; //add number to sum, later squared
+			int sqare = i * i; //square num
+			sumSquares += sqare; //add to the sum of squared numbers
+		}
+		
+		squareSum = squareSum * squareSum; //square the sum
+		int difference = squareSum - sumSquares; //take difference
+		System.out.println(difference); //here's our answer
+	}
+	
+}
